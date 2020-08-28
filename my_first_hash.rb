2456f25592f7a25c4dd315c182b9471c3176569f
Lisa Walters <campi_l@fortlewis.edu>
2020-08-28 15:42:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,7 +16,7 @@ end
 def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
+                  "porecelain vases" => 2,
                   "oil paintings" => 3
                  }
 
